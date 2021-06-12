a603700d49db47510617deb784370f78d5f7c679
Min Ren <min.ren@tm-robot.com>
2021-06-12 20:44:40 +0800

    Add Logger by spdlog.

--- Min/src/Min/EntryPoint.h
+++ Min/src/Min/EntryPoint.h
@@ -6,6 +6,10 @@
 
 	int main(int argc, char** argv)
 	{
+		Min::Log::Init();
+		MN_CORE_WARN("Initialized Log!");
+		int a{ 5 };
+		MN_INFO("Hello! Var = {0}", a);
 		auto app = Min::CreateApplication();
 		app->Run();
 		delete app;
---
+++ Min/src/Min/Log.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Min
+{
+	class MIN_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+#define MN_CORE_TRACE(...)	::Min::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define MN_CORE_INFO(...)	::Min::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define MN_CORE_WARN(...)	::Min::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define MN_CORE_ERROR(...)	::Min::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define MN_CORE_FATAL(...)	::Min::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+
+#define MN_TRACE(...)		::Min::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define MN_INFO(...)		::Min::Log::GetClientLogger()->info(__VA_ARGS__)
+#define MN_WARN(...)		::Min::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define MN_ERROR(...)		::Min::Log::GetClientLogger()->error(__VA_ARGS__)
+#define MN_FATAL(...)		::Min::Log::GetClientLogger()->fatal(__VA_ARGS__)
+
